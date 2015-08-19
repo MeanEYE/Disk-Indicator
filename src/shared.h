87736286171a51be9605f6b6d23d082499aec3ba
Mladen Mijatov <meaneye.rcf@gmail.com>
2015-08-19 03:06:35 +0200

    Added shared header file.

---
+++ src/shared.h
@@ -0,0 +1,33 @@
+#ifndef SHARED_INCLUDED
+#define SHARED_INCLUDED
+
+enum notify_method {
+	X_ORG,
+	CONSOLE,
+	THINKPAD
+};
+
+enum indicator_event {
+	READ,
+	WRITE,
+	BOTH
+};
+
+typedef struct {
+	enum notify_method method;
+	enum indicator_event event;
+	char *device;
+
+	// pointer to functions used for notifying user
+	void (*turn_notification_on)(char *indicator);
+	void (*turn_notification_off)(char *indicator);
+
+	// pointer to method config
+	char *config;
+
+	// history
+	unsigned long read_count;
+	unsigned long write_count;
+} Indicator;
+
+#endif
